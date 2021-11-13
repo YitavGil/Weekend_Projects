2029e8dd61f0d2b4c0b766bf8626e24c6aab39f2
YitavGil <93120542+YitavGil@users.noreply.github.com>
2021-11-13 21:10:02 +0200

    Add files via upload

--- Weekend_Project_#3/Ex4.2-Tribonacci.js
+++ Weekend_Project_#3/Ex4.2-Tribonacci.js
@@ -1,9 +1,14 @@
-function fibN(int1, int2, int3, num) {
-    if (num == 0) {
-      return 0;
+/**
+ * 
+ * @param {array} res 
+ * @param {number} num 
+ * @returns {array}
+ */
+function fibN(res, num) {
+    if (num < 4) {
+      return res.slice(0, num);
     }
   
-    let res = [int1, int2, int3];
     for (let i = 0; i < num; i++) {
       if (i > 2) {
         res.push(res[i-1] + res[i-2] + res[i-3])
@@ -12,4 +17,4 @@ function fibN(int1, int2, int3, num) {
     return res 
   }
 
-  console.log(fibN(0, 1, 1, 7))
+  console.log(fibN([0, 1, 1], 7))
